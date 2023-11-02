0bb1b90a2bf62a5aaadc628ddeb4dddc3cddfe08
rodgreg02 <mindera@ME00391.local>
2023-11-02 17:20:50 +0000

    Added mult table, no colors added.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1)left side\n2)left side upsidedown\n3)twosided\n4)twosided upsidedown\n0) quit");
+        System.out.println("1)left side\n2)left side upsidedown\n3)twosided\n4)twosided upsidedown\n9)Multiplication table\n0) quit");
         Scanner input = new Scanner(System.in);
         int rows = 10;
         boolean quit = false;
@@ -45,6 +45,31 @@ public class Main {
                             System.out.print("*");
                         }
                         System.out.println();
+                    }
+                    break;
+                case 9:
+                    int[][] matrixNumb = new int[13][13];
+                    for (int i = 0; i < 13; i++) {
+                        matrixNumb[0][i] = i;
+                        matrixNumb[i][0] = i;
+                    }
+                    for (int i = 1; i < 13; i++) {
+
+                        for (int j = 1; j < 13; j++) {
+                            matrixNumb[i][j] = i * j;
+                        }
+                    }
+                    for (int i = 0; i < 13; i++) {
+                        System.out.println("");
+                        for (int j = 0; j < 13; j++) {
+                            System.out.print(matrixNumb[i][j]);
+                            if(matrixNumb[i][j] >= 10 && matrixNumb[i][j] < 100){
+                                System.out.print(" ");
+                            } else if (matrixNumb[i][j] <= 9) {
+                                System.out.print("  ");
+                            }
+                            System.out.print("|");
+                        }
                     }
                     break;
                 case 0:
